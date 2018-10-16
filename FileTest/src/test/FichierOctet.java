82486ee3de1715ef93bc37177f72eb3b010560c3
pierrepoulouin <pierrepoulouin@gmail.com>
2018-10-16 10:51:53 +0200

    up

--- FileTest/src/test/FichierOctet.java
+++ FileTest/src/test/FichierOctet.java
@@ -8,22 +8,22 @@ import java.io.IOException;
 public class FichierOctet {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		try {
 			FileInputStream is = new FileInputStream("src\\Flots.java");
 			FileOutputStream os = new FileOutputStream("Flots.txt");
-			 byte b;
-			 
+			byte b;
+
 			while((b = (byte) is.read()) != -1)
 				os.write(b);
 			is.close();
 			os.close();
-				
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 
 	}
 
